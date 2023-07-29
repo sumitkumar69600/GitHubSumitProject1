a2ea013d13f59d72b9a622c21ad195c28db29960
kali <kali@kali>
2023-07-29 07:11:18 -0400

    CommitTestTeam

---
+++ src/ReverseString22.java
@@ -0,0 +1,16 @@
+
+public class ReverseString22 {
+	public static void main(String[] args) {
+		
+	
+	String originalStr = "Hello";
+	String reversedStr = "";
+
+	for (int i = 0; i < originalStr.length(); i++) {
+	  reversedStr = originalStr.charAt(i) + reversedStr;
+	}
+
+	System.out.println("Reversed string: "+ reversedStr);
+	System.out.println("Hello Sumit");
+}
+}
